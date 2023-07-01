1e648189433ee38b694a8a758c45bcd4a1b4e3e7
malamaM <mchiluwe@gmail.com>
2023-07-01 09:23:45 +0200

    9. Applications Visible and auto refresh

--- src/_mock/user.js
+++ src/_mock/user.js
@@ -1,27 +1,34 @@
-import { faker } from '@faker-js/faker';
-import { sample } from 'lodash';
+import axios from 'axios';
 
-// ----------------------------------------------------------------------
+const fetchUserData = async () => {
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 
-const users = [...Array(24)].map((_, index) => ({
-  id: faker.datatype.uuid(),
-  avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
-  name: faker.name.fullName(),
-  company: faker.company.name(),
-  isVerified: faker.datatype.boolean(),
-  status: sample(['active', 'banned']),
-  role: sample([
-    'Leader',
-    'Hr Manager',
-    'UI Designer',
-    'UX Designer',
-    'UI/UX Designer',
-    'Project Manager',
-    'Backend Developer',
-    'Full Stack Designer',
-    'Front End Developer',
-    'Full Stack Developer',
-  ]),
-}));
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/applications');
+    const userData = response.data;
 
-export default users;
+    console.log('User Data:', userData);
+
+    // Map the user data to the desired format
+    /* const users = userData.map((user) => ({
+      id: user.id,
+      avatarUrl: `/assets/images/avatars/avatar_${user.id}.jpg`,
+      name: user.customer_name,
+      company: user.organization_name,
+      isVerified: user.opened === "1" ? "Yes" : "No",
+      //status: user.status === "pending" ? "Awaiting Action" : user.status,
+      role: user.Purpose,
+    })); */
+
+    console.log('Populated Users:', users);
+
+    return users;
+  } catch (error) {
+    console.error('User data fetch error:', error);
+    // Handle the error
+    return [];
+  }
+};
+
+export default fetchUserData;
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,7 +1,9 @@
 import { Helmet } from 'react-helmet-async';
 import { filter } from 'lodash';
 import { sentenceCase } from 'change-case';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 // @mui
 import {
   Card,
@@ -26,18 +28,16 @@ import {
 import Label from '../components/label';
 import Iconify from '../components/iconify';
 import Scrollbar from '../components/scrollbar';
-// sections
-import { UserListHead, UserListToolbar } from '../sections/@dashboard/user';
-// mock
-import USERLIST from '../_mock/user';
+import { UserListHead } from '../sections/@dashboard/user';
+
 
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: 'name', label: 'Name', alignRight: false },
-  { id: 'company', label: 'Company', alignRight: false },
-  { id: 'role', label: 'Role', alignRight: false },
-  { id: 'isVerified', label: 'Verified', alignRight: false },
+  { id: 'customer_name', label: 'Name', alignRight: false },
+  { id: 'organization_name', label: 'Company', alignRight: false },
+  { id: 'shortcode_applied', label: 'Role', alignRight: false },
+  { id: 'opened', label: 'Verified', alignRight: false },
   { id: 'status', label: 'Status', alignRight: false },
   { id: '' },
 ];
@@ -68,25 +68,46 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    return filter(array, (_user) => _user.customer_name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
   }
   return stabilizedThis.map((el) => el[0]);
 }
 
 export default function UserPage() {
   const [open, setOpen] = useState(null);
-
   const [page, setPage] = useState(0);
-
   const [order, setOrder] = useState('asc');
-
   const [selected, setSelected] = useState([]);
-
-  const [orderBy, setOrderBy] = useState('name');
-
+  const [orderBy, setOrderBy] = useState('customer_name');
   const [filterName, setFilterName] = useState('');
-
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [userList, setUserList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+// ...
+
+useEffect(() => {
+  const fetchUsers = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('http://127.0.0.1:8000/api/applications');
+      const userData = response.data;
+      setUserList(userData);
+    } catch (error) {
+      console.error('User data fetch error:', error);
+      // Handle the error
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const intervalId = setInterval(fetchUsers, 10000); // Fetch data every 10 seconds
+
+  return () => {
+    clearInterval(intervalId); // Clean up the interval on component unmount
+  };
+}, []);
+
 
   const handleOpenMenu = (event) => {
     setOpen(event.currentTarget);
@@ -104,25 +125,30 @@ export default function UserPage() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = USERLIST.map((n) => n.name);
+      const newSelecteds = userList.map((n) => n.id);
       setSelected(newSelecteds);
       return;
     }
     setSelected([]);
   };
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
+  const handleClick = (event, customer_name) => {
+    const selectedIndex = selected.indexOf(customer_name);
     let newSelected = [];
+
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = newSelected.concat(selected, customer_name);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
       newSelected = newSelected.concat(selected.slice(0, -1));
     } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
     }
+
     setSelected(newSelected);
   };
 
@@ -131,164 +157,149 @@ export default function UserPage() {
   };
 
   const handleChangeRowsPerPage = (event) => {
-    setPage(0);
     setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   const handleFilterByName = (event) => {
-    setPage(0);
     setFilterName(event.target.value);
   };
 
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - userList.length) : 0;
 
-  const filteredUsers = applySortFilter(USERLIST, getComparator(order, orderBy), filterName);
+  const filteredUsers = applySortFilter(userList, getComparator(order, orderBy), filterName);
 
-  const isNotFound = !filteredUsers.length && !!filterName;
+  const isUserNotFound = filteredUsers.length === 0;
 
   return (
-    <>
+    <Container>
       <Helmet>
-        <title> Applications | Zicta USSD Admin Dashboard </title>
+        <title>User | Management</title>
       </Helmet>
 
-      <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
-            Applications
-          </Typography>
-          <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
-            New Application
-          </Button>
-        </Stack>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          Users
+        </Typography>
+        <Button variant="contained">Add user</Button>
+      </Stack>
 
-        <Card>
-          <UserListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
-
-          <Scrollbar>
-            <TableContainer sx={{ minWidth: 800 }}>
-              <Table>
-                <UserListHead
-                  order={order}
-                  orderBy={orderBy}
-                  headLabel={TABLE_HEAD}
-                  rowCount={USERLIST.length}
-                  numSelected={selected.length}
-                  onRequestSort={handleRequestSort}
-                  onSelectAllClick={handleSelectAllClick}
-                />
-                <TableBody>
-                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                    const { id, name, role, status, company, avatarUrl, isVerified } = row;
-                    const selectedUser = selected.indexOf(name) !== -1;
+      <Card>
+        <Scrollbar>
+          <TableContainer sx={{ minWidth: 800 }}>
+            <Table>
+              <UserListHead
+                order={order}
+                orderBy={orderBy}
+                headLabel={TABLE_HEAD}
+                rowCount={userList.length}
+                numSelected={selected.length}
+                onRequestSort={handleRequestSort}
+                onSelectAllClick={handleSelectAllClick}
+                onFilterName={handleFilterByName}
+              />
+              <TableBody>
+                {filteredUsers
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    const { id, customer_name, shortcode_applied, organization_name, opened, status } = row;
+                    const isItemSelected = selected.indexOf(customer_name) !== -1;
 
                     return (
-                      <TableRow hover key={id} tabIndex={-1} role="checkbox" selected={selectedUser}>
+                      <TableRow
+                        hover
+                        key={id}
+                        tabIndex={-1}
+                        shortcode_applied="checkbox"
+                        selected={isItemSelected}
+                        aria-checked={isItemSelected}
+                      >
                         <TableCell padding="checkbox">
-                          <Checkbox checked={selectedUser} onChange={(event) => handleClick(event, name)} />
+                          <Checkbox
+                            checked={isItemSelected}
+                            onChange={(event) => handleClick(event, customer_name)}
+                          />
                         </TableCell>
-
                         <TableCell component="th" scope="row" padding="none">
                           <Stack direction="row" alignItems="center" spacing={2}>
-                            <Avatar alt={name} src={avatarUrl} />
+                            <Avatar alt={customer_name} src="/static/mock-images/avatars/avatar_1.jpg" />
                             <Typography variant="subtitle2" noWrap>
-                              {name}
+                              {customer_name}
                             </Typography>
                           </Stack>
                         </TableCell>
-
-                        <TableCell align="left">{company}</TableCell>
-
-                        <TableCell align="left">{role}</TableCell>
-
-                        <TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell>
-
+                        <TableCell align="left">{organization_name}</TableCell>
+                        <TableCell align="left">{shortcode_applied}</TableCell>
                         <TableCell align="left">
-                          <Label color={(status === 'banned' && 'error') || 'success'}>{sentenceCase(status)}</Label>
+  <Label
+    variant="ghost"
+    color={opened ? 'success' : 'error'}
+    sx={{ textTransform: 'capitalize' }}
+  >
+    {opened}
+  </Label>
+</TableCell>
+                        <TableCell align="left">
+                          <Label
+                            variant="ghost"
+                            color={(status === 'active' && 'success') || 'error'}
+                            sx={{ textTransform: 'capitalize' }}
+                          >
+                            {sentenceCase(status)}
+                          </Label>
                         </TableCell>
-
                         <TableCell align="right">
-                          <IconButton size="large" color="inherit" onClick={handleOpenMenu}>
-                            <Iconify icon={'eva:more-vertical-fill'} />
+                          <IconButton
+                            aria-controls="user-menu"
+                            aria-haspopup="true"
+                            onClick={handleOpenMenu}
+                          >
+                            <Iconify icon="eva:more-vertical-fill" />
                           </IconButton>
+                          <Popover
+                            open={Boolean(open)}
+                            anchorEl={open}
+                            onClose={handleCloseMenu}
+                            PaperProps={{
+                              sx: { width: 200, py: 1, px: 0 },
+                            }}
+                            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                          >
+                            <MenuItem onClick={handleCloseMenu}>Profile</MenuItem>
+                            <MenuItem onClick={handleCloseMenu}>Email</MenuItem>
+                            <MenuItem onClick={handleCloseMenu}>Block</MenuItem>
+                          </Popover>
                         </TableCell>
                       </TableRow>
                     );
                   })}
-                  {emptyRows > 0 && (
-                    <TableRow style={{ height: 53 * emptyRows }}>
-                      <TableCell colSpan={6} />
-                    </TableRow>
-                  )}
-                </TableBody>
-
-                {isNotFound && (
-                  <TableBody>
-                    <TableRow>
-                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                        <Paper
-                          sx={{
-                            textAlign: 'center',
-                          }}
-                        >
-                          <Typography variant="h6" paragraph>
-                            Not found
-                          </Typography>
-
-                          <Typography variant="body2">
-                            No results found for &nbsp;
-                            <strong>&quot;{filterName}&quot;</strong>.
-                            <br /> Try checking for typos or using complete words.
-                          </Typography>
-                        </Paper>
-                      </TableCell>
-                    </TableRow>
-                  </TableBody>
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
                 )}
-              </Table>
-            </TableContainer>
-          </Scrollbar>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Scrollbar>
 
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={USERLIST.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Card>
-      </Container>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={userList.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Card>
 
-      <Popover
-        open={Boolean(open)}
-        anchorEl={open}
-        onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        PaperProps={{
-          sx: {
-            p: 1,
-            width: 140,
-            '& .MuiMenuItem-root': {
-              px: 1,
-              typography: 'body2',
-              borderRadius: 0.75,
-            },
-          },
-        }}
-      >
-        <MenuItem>
-          <Iconify icon={'eva:edit-fill'} sx={{ mr: 2 }} />
-          Edit
-        </MenuItem>
-
-        <MenuItem sx={{ color: 'error.main' }}>
-          <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
-          Delete
-        </MenuItem>
-      </Popover>
-    </>
+      {isUserNotFound && (
+        <Typography variant="h6" sx={{ mt: 3 }}>
+          No user found
+        </Typography>
+      )}
+    </Container>
   );
 }
