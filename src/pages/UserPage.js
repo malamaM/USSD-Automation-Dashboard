6363825e18f865b1224e27a548df8771eca4b7d4
malamaM <mchiluwe@gmail.com>
2023-07-05 16:45:53 +0200

    16.0 clickable applications Table

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,11 +1,9 @@
 import { Helmet } from 'react-helmet-async';
 import { useNavigate, BrowserRouter } from 'react-router-dom';
-
 import { filter } from 'lodash';
 import { sentenceCase } from 'change-case';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
 
 // @mui
 import {
@@ -30,6 +28,9 @@ import {
   TablePagination,
   CircularProgress,
   useTheme,
+  Dialog,
+  DialogTitle,
+  DialogContent
 } from '@mui/material';
 
 // components
@@ -52,37 +53,34 @@ const createData = (appId, custName, shortCode, expiryDate, licenseStatus) => {
   return { appId, custName, shortCode, expiryDate, licenseStatus };
 };
 
-
 const HandleButtonClick = () => {
-  const navigate = useNavigate(); // Initialize the navigate function
-  navigate('/apply'); // Use the navigate function to redirect
+  const navigate = useNavigate();
+  navigate('/apply');
 };
 
 export default function UserPage() {
   const theme = useTheme();
   const [rows, setRows] = useState([]);
   const [filteredRows, setFilteredRows] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [open, setOpen] = useState(false);
+
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
- 
-
-  
-
-  
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-  
+
   const handleSearch = (event) => {
     const query = event.target.value.toLowerCase();
     setSearchQuery(query);
-  
+
     const filtered = rows.filter(
       (row) =>
         row.appId.toLowerCase().includes(query) ||
@@ -94,7 +92,6 @@ export default function UserPage() {
     setFilteredRows(filtered);
     setPage(0);
   };
- 
 
   useEffect(() => {
     const fetchApplications = async () => {
@@ -102,7 +99,6 @@ export default function UserPage() {
         const response = await axios.get('http://127.0.0.1:8000/api/applications');
         const appData = response.data;
 
-        // Transform appData into rows array
         const newRows = appData.map((data) =>
           createData(
             data.customer_name,
@@ -120,13 +116,21 @@ export default function UserPage() {
     };
 
     fetchApplications();
-    const intervalId = setInterval(fetchApplications, 10000); // Fetch every 10 seconds
+    const intervalId = setInterval(fetchApplications, 10000);
 
     return () => {
-      clearInterval(intervalId); // Cleanup the interval on component unmount
+      clearInterval(intervalId);
     };
   }, []);
 
+  const handleRowClick = (row) => {
+    setSelectedRow(row);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Container>
@@ -138,70 +142,80 @@ export default function UserPage() {
         <Typography variant="h4" sx={{ mb: 2, color: 'black' }}>
           Applications
         </Typography>
-        <Button variant="contained" onClick={HandleButtonClick} sx={{
-  color: 'black',
-  '&:hover': {
-    color: 'white',
-  },
-}}>
-  Add New Application
-</Button>
+        <Button variant="contained" onClick={HandleButtonClick} sx={{ color: 'black', '&:hover': { color: 'white' } }}>
+          Add New Application
+        </Button>
       </Stack>
 
-      
-          
       <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TextField
-        label="Search"
-        value={searchQuery}
-        onChange={handleSearch}
-        margin="normal"
-        variant="outlined"
-        fullWidth
-      />
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align="left"
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(searchQuery ? filteredRows : rows)
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.appId}>
-                    <TableCell>{row.appId}</TableCell>
-                    <TableCell>{row.custName}</TableCell>
-                    <TableCell>{row.shortCode}</TableCell>
-                    <TableCell>{row.expiryDate}</TableCell>
-                    <TableCell>{row.licenseStatus}</TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={(searchQuery ? filteredRows : rows).length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
-      
+        <TextField
+          label="Search"
+          value={searchQuery}
+          onChange={handleSearch}
+          margin="normal"
+          variant="outlined"
+          fullWidth
+        />
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell key={column.id} align="left" style={{ minWidth: column.minWidth }}>
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(searchQuery ? filteredRows : rows)
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.appId}
+                      onClick={() => handleRowClick(row)}
+                    >
+                      <TableCell>{row.appId}</TableCell>
+                      <TableCell>{row.custName}</TableCell>
+                      <TableCell>{row.shortCode}</TableCell>
+                      <TableCell>{row.expiryDate}</TableCell>
+                      <TableCell>{row.licenseStatus}</TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={(searchQuery ? filteredRows : rows).length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Application Details</DialogTitle>
+        <DialogContent>
+          {selectedRow && (
+            <>
+              <div>Customer Name: {selectedRow.appId}</div>
+              <div>Organization: {selectedRow.custName}</div>
+              <div>Short Code: {selectedRow.shortCode}</div>
+              <div>Status: {selectedRow.expiryDate}</div>
+              <div>Opened: {selectedRow.licenseStatus}</div>
+            </>
+          )}
+          <Button onClick={handleClose}>Close</Button>
+        </DialogContent>
+      </Dialog>
     </Container>
   );
 }
